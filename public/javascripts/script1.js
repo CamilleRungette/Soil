7ca82fc76046c710ef433d9fe5bd89a12d7ff50e
CamilleRungette <c.rungette@gmail.com>
2020-05-19 12:38:36 +0200

    automatic carrousell ok

--- public/javascripts/script1.js
+++ public/javascripts/script1.js
@@ -13,18 +13,42 @@
 
 $('#screen2-1').hide();
 $('#screen3-1').hide();
-
-$('#screen2-2').hide();
-$('#screen3-2').hide();
+$('#screen4-1').hide();
+$('#screen5-1').hide();
+$('#screen6-1').hide();
+$('#screen7-1').hide();
+$('#screen8-1').hide();
 
 let i = 1;
 setInterval(function() {
-  console.log(i);  
-  $(`#screen${i}-1`).fadeOut("slow").next().fadeIn("slow");
-  $(`#screen${i}-2`).delay(100).fadeOut("slow").next().fadeIn("slow");
+  console.log($(`#screen8-2`).attr('class')[0]);
+  
+
+  if($(`#screen${i}-2`).attr('class')[0] != "n"){
+
+      $(`#screen${i}-1`).fadeOut("slow")
+      setTimeout(function(){
+        $('.screen1').css("width", "100%")
+        $('.screen2').css("width", "0%")
+      }, 500)
+      $(`#screen${i}-1`).next().fadeIn(1500);
+      $(`#screen${i}-2`).fadeOut("slow").next().fadeIn(1500);
+
+  }else {
+
+    $(`#screen${i}-1`).fadeOut("slow");
+    setTimeout(function(){
+      $('.screen1').css("width", "50%")
+      $('.screen2').css("width", "50%")
+    }, 500)
+    $(`#screen${i}-1`).next().fadeIn(1500)
+    $(`#screen${i}-2`).fadeOut("slow").next().fadeIn(1500);
+  
+  }  
+  
   
   i = i + 1;
-  if (i === 4) {
+  if (i === 9) {
     i = 1;
   }
 }, 4000);
